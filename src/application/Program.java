0c0060d9b6c1b6a6e44eda9b0fbabe08e0491283
Mandorik <arthur_jakobi@hotmail.com>
2025-08-28 21:29:36 -0300

    Implementing special move Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,12 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter the type of piece it'll be promoted (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
